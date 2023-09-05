c5426c874eba7999919ecb1d9344e95ca19780a6
cherkasskypavel <fenderik09@mail.ru>
2023-09-05 22:28:10 +0300

    Added login_page.py and new locators class for login page.

---
+++ pages/login_page.py
@@ -0,0 +1,16 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert 'url' in self.browser.current_url, 'URL страницы логина некорректный'
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Отсутствует форма логина'
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Отсутствует форма регистрации'
--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,14 @@
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     page = MainPage(browser, link)
     page.open()
-    page.go_to_login_page()
+
 
 def test_guest_can_see_login_button(browser):
-    link = 'http://selenium1py.pythonanywhere.com/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_page()
