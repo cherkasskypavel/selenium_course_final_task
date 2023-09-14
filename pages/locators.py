5aec3e4ab9cd08da941d68c32329a4bd00d6b05d
cherkasskypavel <fenderik09@mail.ru>
2023-09-14 23:41:30 +0300

    Step 14. Линки вынес в начало файла. Осталось перепроверить корректность всех тестов, XFAILED.

--- pages/locators.py
+++ pages/locators.py
@@ -1,17 +1,5 @@
 from selenium.webdriver.common.by import By
-import pytest
 class LinksToTest():
-    LINK_LIST = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                               marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
     LINK = 'https://selenium1py.pythonanywhere.com/'
     LOGIN_PAGE_LINK = 'http://selenium1py.pythonanywhere.com/accounts/login/'
 class BasePageLocators():
